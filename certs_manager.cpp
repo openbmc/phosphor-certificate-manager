72faf4e012b73f8b765cc47c57d9ce56d1c6dd58
Jayanth Othayoth <ojayanth@in.ibm.com>
2018-10-16 07:02:01 -0500

    Added InstallCompleted signal
    "InstallCompleted" signal is used for indicating the status of the
    certificate installation steps. Emit signal after the sucessfull
    certificate installation steps.
    
    Change-Id: Ib7485a4b99db793c12c627fc1797c94d64d818cb
    Signed-off-by: Jayanth Othayoth <ojayanth@in.ibm.com>

--- certs_manager.cpp
+++ certs_manager.cpp
@@ -75,6 +75,9 @@ void Manager::install(const std::string path)
     {
         reloadOrReset(unit);
     }
+
+    // Emit signal
+    this->installCompleted();
 }
 
 void Manager::serverInstallHelper(const std::string& filePath)
