20c9af7a53cce6185dd34ddd2b7c08eaffbc184c
Marri Devender Rao <devenrao@in.ibm.com>
2019-02-27 08:41:07 -0600

    Create cerificate object during startup for existing certificate
    During service start check if certificate file already exist if so
    load the certificate file, validate and create certificate object
    
    Change-Id: If0d62cc52fa34b8992b63fc49ed8014280b3e469
    Signed-off-by: Marri Devender Rao <devenrao@in.ibm.com>

--- certs_manager.cpp
+++ certs_manager.cpp
@@ -3,14 +3,14 @@
 #include <phosphor-logging/elog-errors.hpp>
 #include <xyz/openbmc_project/Certs/Install/error.hpp>
 #include <xyz/openbmc_project/Common/error.hpp>
-
 namespace phosphor
 {
 namespace certs
 {
 
-using InternalFailure =
-    sdbusplus::xyz::openbmc_project::Common::Error::InternalFailure;
+using namespace sdbusplus::xyz::openbmc_project::Common::Error;
+using InvalidCertificate =
+    sdbusplus::xyz::openbmc_project::Certs::Install::Error::InvalidCertificate;
 using Reason = xyz::openbmc_project::Certs::Install::InvalidCertificate::REASON;
 
 /** @brief Constructor to put object onto bus at a dbus path.
@@ -27,6 +27,31 @@ Manager::Manager(sdbusplus::bus::bus& bus, const char* path,
     bus(bus), objectPath(path), certType(type), unitToRestart(std::move(unit)),
     certInstallPath(std::move(installPath))
 {
+    if (fs::exists(certInstallPath))
+    {
+        try
+        {
+            // TODO: Issue#3 At present supporting only one certificate to be
+            // uploaded this need to be revisited to support multiple
+            // certificates
+            auto certObjectPath = objectPath + '/' + '1';
+            certificatePtr = std::make_unique<Certificate>(
+                bus, certObjectPath, certType, unitToRestart, certInstallPath,
+                certInstallPath);
+            certificatePtr->install(certInstallPath);
+        }
+        catch (const InternalFailure& e)
+        {
+            certificatePtr.reset(nullptr);
+            report<InternalFailure>();
+        }
+        catch (const InvalidCertificate& e)
+        {
+            certificatePtr.reset(nullptr);
+            report<InvalidCertificate>(
+                Reason("Existing certificate file is corrupted"));
+        }
+    }
 }
 
 void Manager::install(const std::string filePath)
