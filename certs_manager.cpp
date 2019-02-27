9c3a82bdd92b5c5fc548ebf861ad1ce7d9a1ae84
Marri Devender Rao <devenrao@in.ibm.com>
2019-02-27 08:47:12 -0600

    Create D-Bus Certificate object during certificate install
    During certificate upload through REST or through POST on
    CertificateCollection create certificate object, validate
    certificate and copy certficate to the system.
    
    Tested:
    1. Verified certificate object is created if certificate exists
    2. Verified certificate object is created during install
    3. Verified certificate properties
    Change-Id: If31aa939c9cb75b5d683a7614ddc55ad38297874
    Signed-off-by: Marri Devender Rao <devenrao@in.ibm.com>

--- certs_manager.cpp
+++ certs_manager.cpp
@@ -9,8 +9,7 @@ namespace phosphor
 namespace certs
 {
 
-using InternalFailure =
-    sdbusplus::xyz::openbmc_project::Common::Error::InternalFailure;
+using namespace sdbusplus::xyz::openbmc_project::Common::Error;
 using InvalidCertificate =
     sdbusplus::xyz::openbmc_project::Certs::Install::Error::InvalidCertificate;
 using Reason = xyz::openbmc_project::Certs::Install::InvalidCertificate::REASON;
@@ -54,6 +53,19 @@ Manager::Manager(sdbusplus::bus::bus& bus, const char* path,
 
 void Manager::install(const std::string filePath)
 {
+    using Argument = xyz::openbmc_project::Common::NotAllowed;
+    log<level::INFO>("Manager install certificate",
+                     entry("FILEPATH=%s", filePath.c_str()));
+    // Supporting only 1 certificate, user can choose to replace
+    // existing certificate by using replace certificate
+    if (certificatePtr != nullptr)
+    {
+        elog<NotAllowed>(Argument::REASON("Certificate already exist"));
+    }
+    auto certObjectPath = objectPath + '/' + "1";
+    certificatePtr =
+        std::make_unique<Certificate>(bus, certObjectPath, certType,
+                                      unitToRestart, certInstallPath, filePath);
 }
 
 void Manager::delete_()
