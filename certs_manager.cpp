a70498071608023d7bf8b106a0ca538b6a9099a6
Marri Devender Rao <devenrao@in.ibm.com>
2019-03-19 05:00:28 -0500

    Add Generate Key and Certificate Signing Request (CSR)
    Generates Private key and CSR file.
    
    -The generateCSR method defined in Create interface is implemented
    by manager class to Create CSR and PrivateKey files.
    
    -The cSR method defined in View interface is implemented by CSR
    class to view CSR file.
    
    - Generate CSR is time consuming operation and it might time-out
    the D-Bus call. Forking process and performing CSR generation in
    the child process, adding the process ID of the child process to the
    SD Event loop so that callback is received when the chid process
    is done with the CSR generation.
    
    - As the GenerateCSR method returns immediately, caller need
    to wait on InterfacesAdded signal that is generated after completion
    of the CSR request. The caller then invokes cSR method of
    CSR interface to read the CSR.
    
    - For any failure in Generate CSR CSR object is created with error
    status.
    
    - CSR object raises exception if error is set else CSR data is
    returned to the caller.
    
    - To cater for failure cases caller need to start a timer, which
    will be terminated after getting InterfaceAdded signal or upon timeout.
    
    -Added Unit tests.
    Tested:
    1) Added unit tests to verify CSR generation
    2) Tested with Redfish to generate and view CSR
    curl -c cjar -b cjar -k -H "X-Auth-Token: $bmc_token" -X POST
    https://${bmc}/redfish/v1/CertificateService/Actions/CertificateService.GenerateCSR/
    -d @generate.jon
    
    {
      "CSRString": "-----BEGIN CERTIFICATE REQUEST---7E=\n-----END CERTIFICATE
    REQUEST-----\n",
      "CertificateCollection": {
        "@odata.id": "/redfish/v1/AccountService/LDAP/Certificates/"
      }
    }
    Change-Id: I1e3ae8df45f87bfd8903f552d93c4df1af7c569f
    Signed-off-by: Marri Devender Rao <devenrao@in.ibm.com>
    Signed-off-by: Nagaraju Goruganti <ngorugan@in.ibm.com>

--- certs_manager.cpp
+++ certs_manager.cpp
@@ -1,29 +1,36 @@
 #include "certs_manager.hpp"
 
+#include <openssl/bio.h>
+#include <openssl/crypto.h>
+#include <openssl/err.h>
+#include <openssl/evp.h>
+#include <openssl/pem.h>
+#include <openssl/x509v3.h>
+#include <unistd.h>
+
 #include <phosphor-logging/elog-errors.hpp>
 #include <xyz/openbmc_project/Certs/error.hpp>
 #include <xyz/openbmc_project/Common/error.hpp>
+
 namespace phosphor
 {
 namespace certs
 {
-
 using InternalFailure =
     sdbusplus::xyz::openbmc_project::Common::Error::InternalFailure;
 
-/** @brief Constructor to put object onto bus at a dbus path.
- *  @param[in] bus - Bus to attach to.
- *  @param[in] path - Path to attach at.
- *  @param[in] type - Type of the certificate.
- *  @param[in] unit - Unit consumed by this certificate.
- *  @param[in] installPath - Certificate installation path.
- */
-Manager::Manager(sdbusplus::bus::bus& bus, const char* path,
-                 const CertificateType& type, UnitsToRestart&& unit,
-                 CertInstallPath&& installPath) :
+using EVP_PKEY_Ptr = std::unique_ptr<EVP_PKEY, decltype(&::EVP_PKEY_free)>;
+using X509_REQ_Ptr = std::unique_ptr<X509_REQ, decltype(&::X509_REQ_free)>;
+using BIO_Ptr = std::unique_ptr<BIO, decltype(&::BIO_free_all)>;
+using BIGNUM_Ptr = std::unique_ptr<BIGNUM, decltype(&::BN_free)>;
+
+Manager::Manager(sdbusplus::bus::bus& bus, sdeventplus::Event& event,
+                 const char* path, const CertificateType& type,
+                 UnitsToRestart&& unit, CertInstallPath&& installPath) :
     Ifaces(bus, path),
-    bus(bus), objectPath(path), certType(type), unitToRestart(std::move(unit)),
-    certInstallPath(std::move(installPath))
+    bus(bus), event(event), objectPath(path), certType(type),
+    unitToRestart(std::move(unit)), certInstallPath(std::move(installPath)),
+    childPtr(nullptr)
 {
     using InvalidCertificate =
         sdbusplus::xyz::openbmc_project::Certs::Error::InvalidCertificate;
@@ -82,5 +89,311 @@ void Manager::delete_()
         certificatePtr.reset(nullptr);
     }
 }
+
+std::string Manager::generateCSR(
+    std::vector<std::string> alternativeNames, std::string challengePassword,
+    std::string city, std::string commonName, std::string contactPerson,
+    std::string country, std::string email, std::string givenName,
+    std::string initials, int64_t keyBitLength, std::string keyCurveId,
+    std::string keyPairAlgorithm, std::vector<std::string> keyUsage,
+    std::string organization, std::string organizationalUnit, std::string state,
+    std::string surname, std::string unstructuredName)
+{
+    // We support only one CSR.
+    csrPtr.reset(nullptr);
+    csrStatus = Status::SUCCESS;
+    auto pid = fork();
+    if (pid == -1)
+    {
+        csrStatus = Status::FAILURE;
+        log<level::ERR>("Error occurred during forking process");
+        report<InternalFailure>();
+    }
+    else if (pid == 0)
+    {
+        try
+        {
+            int ret = 0;
+            // set version of x509 req
+            int nVersion = 1;
+            X509_REQ_Ptr x509Req(X509_REQ_new(), ::X509_REQ_free);
+            ret = X509_REQ_set_version(x509Req.get(), nVersion);
+            if (ret != 1)
+            {
+                log<level::ERR>(
+                    "Error occured during X509_REQ_set_version call");
+                elog<InternalFailure>();
+            }
+
+            // set subject of x509 req
+            X509_NAME* x509Name = X509_REQ_get_subject_name(x509Req.get());
+
+            if (!alternativeNames.empty())
+            {
+                for (auto& name : alternativeNames)
+                {
+                    addEntry(x509Name, "subjectAltName", name);
+                }
+            }
+            addEntry(x509Name, "challengePassword", challengePassword);
+            addEntry(x509Name, "L", city);
+            addEntry(x509Name, "CN", commonName);
+            addEntry(x509Name, "name", contactPerson);
+            addEntry(x509Name, "C", country);
+            addEntry(x509Name, "emailAddress", email);
+            addEntry(x509Name, "GN", givenName);
+            addEntry(x509Name, "initials", initials);
+            addEntry(x509Name, "algorithm", keyPairAlgorithm);
+            if (!keyUsage.empty())
+            {
+                for (auto& usage : keyUsage)
+                {
+                    addEntry(x509Name, "keyUsage", usage);
+                }
+            }
+            addEntry(x509Name, "O", organization);
+            addEntry(x509Name, "ST", state);
+            addEntry(x509Name, "SN", surname);
+            addEntry(x509Name, "unstructuredName", unstructuredName);
+
+            // generate rsa key
+            BIGNUM_Ptr bne(BN_new(), ::BN_free);
+            ret = BN_set_word(bne.get(), RSA_F4);
+            if (ret != 1)
+            {
+                log<level::ERR>("Error occured during BN_set_word call");
+                elog<InternalFailure>();
+            }
+
+            // RSA_Ptr rsa(RSA_new(), ::RSA_free);
+            RSA* rsa = RSA_new();
+            ret = RSA_generate_key_ex(rsa, keyBitLength, bne.get(), NULL);
+            if (ret != 1)
+            {
+                log<level::ERR>("Error occured during RSA_generate_key_ex call",
+                                entry("KEYBITLENGTH=%PRIu64", keyBitLength));
+                elog<InternalFailure>();
+            }
+
+            // set public key of x509 req
+            EVP_PKEY_Ptr pKey(EVP_PKEY_new(), ::EVP_PKEY_free);
+            EVP_PKEY_assign_RSA(pKey.get(), rsa);
+            ret = X509_REQ_set_pubkey(x509Req.get(), pKey.get());
+            if (ret != 1)
+            {
+                log<level::ERR>("Error occured while setting Public key");
+                elog<InternalFailure>();
+            }
+
+            log<level::ERR>("Writing private key to file");
+            // write private key to file
+            std::string path = fs::path(certInstallPath).parent_path();
+            std::string privKeyPath = path + '/' + PRIV_KEY_FILE_NAME;
+
+            FILE* fp = std::fopen(privKeyPath.c_str(), "w");
+            ret = PEM_write_PrivateKey(fp, pKey.get(), NULL, NULL, 0, 0, NULL);
+            if (fp == NULL)
+            {
+                ret = -1;
+                log<level::ERR>("Error occured creating private key to file");
+                elog<InternalFailure>();
+            }
+            std::fclose(fp);
+            if (ret != 1)
+            {
+                log<level::ERR>(
+                    "Error occured while writing private key to file");
+                elog<InternalFailure>();
+            }
+
+            // set sign key of x509 req
+            ret = X509_REQ_sign(x509Req.get(), pKey.get(), EVP_sha256());
+            if (ret <= 0)
+            {
+                log<level::ERR>("Error occured while signing key of x509");
+                elog<InternalFailure>();
+            }
+            log<level::INFO>("Writing CSR to file");
+            std::string csrFilePath = path + '/' + CSR_FILE_NAME;
+            writeCSR(csrFilePath, x509Req);
+            exit(EXIT_SUCCESS);
+        }
+        catch (const InternalFailure& e)
+        {
+            // commit the error reported in child process and exit
+            // Callback method from SDEvent Loop looks for exit status
+            exit(EXIT_FAILURE);
+            commit<InternalFailure>();
+        }
+    }
+    else
+    {
+        using namespace sdeventplus::source;
+        Child::Callback callback = [this](Child& eventSource,
+                                          const siginfo_t* si) {
+            eventSource.set_enabled(Enabled::On);
+            if (si->si_status != 0)
+            {
+                csrStatus = Status::FAILURE;
+            }
+            this->createCSRObject();
+        };
+        try
+        {
+            sigset_t ss;
+            if (sigemptyset(&ss) < 0)
+            {
+                log<level::ERR>("Unable to initialize signal set");
+                elog<InternalFailure>();
+            }
+            if (sigaddset(&ss, SIGCHLD) < 0)
+            {
+                log<level::ERR>("Unable to add signal to signal set");
+                elog<InternalFailure>();
+            }
+
+            // Block SIGCHLD first, so that the event loop can handle it
+            if (sigprocmask(SIG_BLOCK, &ss, NULL) < 0)
+            {
+                log<level::ERR>("Unable to block signal");
+                elog<InternalFailure>();
+            }
+            if (childPtr)
+            {
+                childPtr.reset();
+            }
+            childPtr = std::make_unique<Child>(event, pid, WEXITED | WSTOPPED,
+                                               std::move(callback));
+        }
+        catch (const InternalFailure& e)
+        {
+            // commit the error reported in process process and exit
+            // Callback method from SDEvent Loop looks for exit status
+            csrStatus = Status::FAILURE;
+            commit<InternalFailure>();
+        }
+    }
+    auto csrObjectPath = objectPath + '/' + "csr";
+    return csrObjectPath;
+}
+
+void Manager::addEntry(X509_NAME* x509Name, const char* field,
+                       const std::string& bytes)
+{
+    if (bytes.empty())
+    {
+        return;
+    }
+    int ret = X509_NAME_add_entry_by_txt(
+        x509Name, field, MBSTRING_ASC,
+        reinterpret_cast<const unsigned char*>(bytes.c_str()), -1, -1, 0);
+    if (ret != 1)
+    {
+        log<level::ERR>("Unable to set entry", entry("FIELD=%s", field),
+                        entry("VALUE=%s", bytes.c_str()));
+        elog<InternalFailure>();
+    }
+}
+
+void Manager::createCSRObject()
+{
+    if (csrPtr)
+    {
+        csrPtr.reset(nullptr);
+    }
+    auto csrObjectPath = objectPath + '/' + "csr";
+    csrPtr = std::make_unique<CSR>(bus, csrObjectPath.c_str(),
+                                   certInstallPath.c_str(), csrStatus);
+}
+
+void Manager::writeCSR(const std::string& filePath, const X509_REQ_Ptr& x509Req)
+{
+    if (fs::exists(filePath))
+    {
+        log<level::INFO>("Removing the existing file",
+                         entry("FILENAME=%s", filePath.c_str()));
+        if (!fs::remove(filePath.c_str()))
+        {
+            log<level::ERR>("Unable to remove the file",
+                            entry("FILENAME=%s", filePath.c_str()));
+            elog<InternalFailure>();
+        }
+    }
+
+    FILE* fp = NULL;
+
+    if ((fp = std::fopen(filePath.c_str(), "w")) == NULL)
+    {
+        log<level::ERR>("Error opening the file to write the CSR",
+                        entry("FILENAME=%s", filePath.c_str()));
+        elog<InternalFailure>();
+    }
+
+    int rc = PEM_write_X509_REQ(fp, x509Req.get());
+    if (!rc)
+    {
+        log<level::ERR>("PEM write routine failed",
+                        entry("FILENAME=%s", filePath.c_str()));
+        std::fclose(fp);
+        elog<InternalFailure>();
+    }
+    std::fclose(fp);
+}
+
+CSR::CSR(sdbusplus::bus::bus& bus, const char* path,
+         CertInstallPath&& installPath, const Status& status) :
+    CSRViewIface(bus, path, true),
+    bus(bus), objectPath(path), certInstallPath(std::move(installPath)),
+    csrStatus(status)
+{
+    // Emit deferred signal.
+    this->emit_object_added();
+}
+
+std::string CSR::cSR()
+{
+    if (csrStatus == Status::FAILURE)
+    {
+        log<level::ERR>("Failure in Generating CSR");
+        elog<InternalFailure>();
+    }
+    std::string path = fs::path(certInstallPath).parent_path();
+    std::string csrFilePath = path + '/' + CSR_FILE_NAME;
+    if (!fs::exists(csrFilePath.c_str()))
+    {
+        log<level::ERR>("CSR file doesn't exists",
+                        entry("FILENAME=%s", csrFilePath.c_str()));
+        elog<InternalFailure>();
+    }
+
+    FILE* fp = std::fopen(csrFilePath.c_str(), "r");
+    X509_REQ_Ptr x509Req(PEM_read_X509_REQ(fp, NULL, NULL, NULL),
+                         ::X509_REQ_free);
+    if (x509Req == NULL || fp == NULL)
+    {
+        if (fp != NULL)
+        {
+            std::fclose(fp);
+        }
+        log<level::ERR>("ERROR occured while reading CSR file",
+                        entry("FILENAME=%s", csrFilePath.c_str()));
+        elog<InternalFailure>();
+    }
+    std::fclose(fp);
+
+    BIO_Ptr bio(BIO_new(BIO_s_mem()), ::BIO_free_all);
+    int ret = PEM_write_bio_X509_REQ(bio.get(), x509Req.get());
+    if (ret <= 0)
+    {
+        log<level::ERR>("Error occured while calling PEM_write_bio_X509_REQ");
+        elog<InternalFailure>();
+    }
+
+    BUF_MEM* mem = NULL;
+    BIO_get_mem_ptr(bio.get(), &mem);
+    std::string pem(mem->data, mem->length);
+    return pem;
+}
+
 } // namespace certs
 } // namespace phosphor
--- certs_manager.hpp
+++ certs_manager.hpp
@@ -1,6 +1,14 @@
 #pragma once
+#include "config.h"
+
 #include "certificate.hpp"
 
+#include <openssl/x509v3.h>
+
+#include <sdeventplus/source/child.hpp>
+#include <sdeventplus/source/event.hpp>
+#include <xyz/openbmc_project/Certs/CSR/Create/server.hpp>
+#include <xyz/openbmc_project/Certs/CSR/server.hpp>
 #include <xyz/openbmc_project/Certs/Install/server.hpp>
 #include <xyz/openbmc_project/Object/Delete/server.hpp>
 
@@ -8,9 +16,22 @@ namespace phosphor
 {
 namespace certs
 {
-using Create = sdbusplus::xyz::openbmc_project::Certs::server::Install;
+using Install = sdbusplus::xyz::openbmc_project::Certs::server::Install;
 using Delete = sdbusplus::xyz::openbmc_project::Object::server::Delete;
-using Ifaces = sdbusplus::server::object::object<Create, Delete>;
+using CSRCreate = sdbusplus::xyz::openbmc_project::Certs::CSR::server::Create;
+using CSRView = sdbusplus::xyz::openbmc_project::Certs::server::CSR;
+using Ifaces = sdbusplus::server::object::object<Install, CSRCreate, Delete>;
+
+using CSRViewIface = sdbusplus::server::object::object<CSRView>;
+
+using X509_REQ_Ptr = std::unique_ptr<X509_REQ, decltype(&::X509_REQ_free)>;
+class CSR;
+
+enum class Status
+{
+    SUCCESS,
+    FAILURE,
+};
 
 class Manager : public Ifaces
 {
@@ -34,14 +55,15 @@ class Manager : public Ifaces
 
     /** @brief Constructor to put object onto bus at a dbus path.
      *  @param[in] bus - Bus to attach to.
+     *  @param[in] event - sd event handler.
      *  @param[in] path - Path to attach at.
      *  @param[in] type - Type of the certificate.
      *  @param[in] unit - Unit consumed by this certificate.
      *  @param[in] installPath - Certificate installation path.
      */
-    Manager(sdbusplus::bus::bus& bus, const char* path,
-            const CertificateType& type, UnitsToRestart&& unit,
-            CertInstallPath&& installPath);
+    Manager(sdbusplus::bus::bus& bus, sdeventplus::Event& event,
+            const char* path, const CertificateType& type,
+            UnitsToRestart&& unit, CertInstallPath&& installPath);
 
     /** @brief Implementation for Install
      *  Replace the existing certificate key file with another
@@ -56,9 +78,110 @@ class Manager : public Ifaces
      */
     void delete_() override;
 
+    /** @brief Implementation for GenerateCSR
+     *
+     *  @param[in] alternativeNames - Additional hostnames of the component that
+     *  is being secured.
+     *  @param[in] challengePassword - The challenge password to be applied to
+     *  the certificate for revocation requests.
+     *  @param[in] city - The city or locality of the organization making the
+     *  request. For Example Austin
+     *  @param[in] commonName - The fully qualified domain name of the component
+     *  that is being secured.
+     *  @param[in] contactPerson - The name of the user making the request.
+     *  @param[in] country - The country of the organization making the request.
+     *  @param[in] email - The email address of the contact within the
+     *  organization making the request. Email validtaion does not perform
+     *  authentication to validate the email address. Only check the given email
+     *  format is valid for an email address.
+     *  @param[in] givenName - The given name of the user making the request.
+     *  @param[in] initials - The initials of the user making the request.
+     *  @param[in] keyBitLength - The length of the key in bits, if needed based
+                   on the value of the KeyPairAlgorithm parameter.
+     *  @param[in] keyCurveId - The curve ID to be used with the key, if needed
+     *  based on the value of the KeyPairAlgorithm parameter.
+     *  @param[in] keyPairAlgorithm - The type of key pair for use with signing
+     *  algorithms. Valid built-in algorithm names for private key generation
+     *  are: RSA and EC.
+     *  @param[in] keyUsage - Key usage extensions define the purpose of the
+     *  public key contained in a certificate. Valid Key usage extensions and
+     *  its usage description.
+     *  - ClientAuthentication: The public key is used for TLS WWW client
+     *    authentication.
+     *  - CodeSigning: The public key is used for the signing of executable code
+     *  - CRLSigning: The public key is used for verifying signatures on
+     *    certificate revocation lists (CLRs).
+     *  - DataEncipherment: The public key is used for directly enciphering
+     *    raw user data without the use of an intermediate symmetric cipher.
+     *  - DecipherOnly: The public key could be used for deciphering data while
+     *    performing key agreement.
+     *  - DigitalSignature: The public key is used for verifying digital
+     *    signatures, other than signatures on certificatesand CRLs.
+     *  - EmailProtection: The public key is used for email protection.
+     *  - EncipherOnly: Thepublic key could be used for enciphering data while
+     *    performing key agreement.
+     *  - KeyCertSign: The public key is used for verifying signatures on
+     *    public key certificates.
+     *  - KeyEncipherment: The public key is used for enciphering private or
+     *    secret keys.
+     *  - NonRepudiation: The public key is used to verify digital signatures,
+     *    other than signatures on certificates and CRLs, and used to provide
+     *    a non- repudiation service that protects against the signing entity
+     *    falsely denying some action.
+     *  - OCSPSigning: The public key is used for signing OCSP responses.
+     *  - ServerAuthentication: The public key is used for TLS WWW server
+     *    authentication.
+     *  - Timestamping: The public key is used for binding the hash of an
+     *    object to a time.
+     *  @param[in] organization - The legal name of the organization. This
+     *  should not be abbreviated and should include suffixes such as Inc,Corp,
+     *  or LLC.For example, IBM Corp.
+     *  @param[in] organizationalUnit - The name of the unit or division of the
+     *  organization making the request.
+     *  @param[in] state - The state or province where the organization is
+     *  located. This should not be abbreviated. For example, Texas.
+     *  @param[in] surname - The surname of the user making the request.
+     *  @param[in] unstructuredName - The unstructured name of the subject.
+     *
+     *  @return path[std::string] - The object path of the D-Bus object
+     *  representing CSR string. Note: For new CSR request will update the
+     *  existing CSR in the system.
+     */
+    std::string generateCSR(
+        std::vector<std::string> alternativeNames,
+        std::string challengePassword, std::string city, std::string commonName,
+        std::string contactPerson, std::string country, std::string email,
+        std::string givenName, std::string initials, int64_t keyBitLength,
+        std::string keyCurveId, std::string keyPairAlgorithm,
+        std::vector<std::string> keyUsage, std::string organization,
+        std::string organizationalUnit, std::string state, std::string surname,
+        std::string unstructuredName) override;
+
   private:
+    /** @brief Add the specified CSR filed with the data
+     *  @param[in] x509Name Structure used in setting certificate properties
+     *  @param[in] field field name
+     *  @param[in] bytes field value in bytes
+     */
+    void addEntry(X509_NAME* x509Name, const char* field,
+                  const std::string& bytes);
+
+    /** @brief Create CSR D-Bus object by reading the data in the CSR file
+     */
+    void createCSRObject();
+
+    /** @brief Write generated CSR data to file
+     *
+     *  @param[in] filePath CSR file path.
+     *  @param[in] x509Req OpenSSL Request Pointer.
+     */
+    void writeCSR(const std::string& filePath, const X509_REQ_Ptr& x509Req);
+
     /** @brief sdbusplus handler */
     sdbusplus::bus::bus& bus;
+
+    // sdevent Event handle
+    sdeventplus::Event& event;
 
     /** @brief object path */
     std::string objectPath;
@@ -74,7 +197,54 @@ class Manager : public Ifaces
 
     /** @brief pointer to certificate */
     std::unique_ptr<Certificate> certificatePtr = nullptr;
+
+    /** @brief pointer to CSR */
+    std::unique_ptr<CSR> csrPtr = nullptr;
+
+    /** @brief SDEventPlus child pointer added to event loop */
+    std::unique_ptr<sdeventplus::source::Child> childPtr;
+
+    /** @brief Status of GenerateCSR request */
+    Status csrStatus;
 };
 
+/** @class CSR
+ *  @brief To view CSR certificates
+ */
+class CSR : public CSRViewIface
+{
+  public:
+    CSR() = delete;
+    ~CSR() = default;
+    CSR(const CSR&) = delete;
+    CSR& operator=(const CSR&) = delete;
+    CSR(CSR&&) = default;
+    CSR& operator=(CSR&&) = default;
+
+    /** @brief Constructor to put object onto bus at a D-Bus path.
+     *  @param[in] bus - Bus to attach to.
+     *  @param[in] path - The D-Bus object path to attach at.
+     *  @param[in] installPath - Certificate installation path.
+     *  @param[in] status - Status of Generate CSR request
+     */
+    CSR(sdbusplus::bus::bus& bus, const char* path,
+        CertInstallPath&& installPath, const Status& status);
+    /** @brief Return CSR
+     */
+    std::string cSR() override;
+
+  private:
+    /** @brief sdbusplus handler */
+    sdbusplus::bus::bus& bus;
+
+    /** @brief object path */
+    std::string objectPath;
+
+    /** @brief Certificate file installation path **/
+    CertInstallPath certInstallPath;
+
+    /** @brief Status of GenerateCSR request */
+    Status csrStatus;
+};
 } // namespace certs
 } // namespace phosphor
