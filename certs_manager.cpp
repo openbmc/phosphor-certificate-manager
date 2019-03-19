725fc5a269e20524c3e170873f46452792e2664b
Marri Devender Rao <devenrao@in.ibm.com>
2019-03-19 05:00:28 -0500

    Cater for GenerateCSR Redfish CertificateService action
    Change-Id: I1e3ae8df45f87bfd8903f552d93c4df1af7c569f
    Signed-off-by: Marri Devender Rao <devenrao@in.ibm.com>

--- certs_manager.cpp
+++ certs_manager.cpp
@@ -1,5 +1,12 @@
 #include "certs_manager.hpp"
 
+#include <openssl/bio.h>
+#include <openssl/crypto.h>
+#include <openssl/err.h>
+#include <openssl/evp.h>
+#include <openssl/pem.h>
+#include <openssl/x509v3.h>
+
 #include <phosphor-logging/elog-errors.hpp>
 #include <xyz/openbmc_project/Certs/Install/error.hpp>
 #include <xyz/openbmc_project/Common/error.hpp>
@@ -10,6 +17,11 @@ namespace certs
 
 using InternalFailure =
     sdbusplus::xyz::openbmc_project::Common::Error::InternalFailure;
+
+using EVP_PKEY_Ptr = std::unique_ptr<EVP_PKEY, decltype(&::EVP_PKEY_free)>;
+using X509_REQ_Ptr = std::unique_ptr<X509_REQ, decltype(&::X509_REQ_free)>;
+using BIO_Ptr = std::unique_ptr<BIO, decltype(&::BIO_free_all)>;
+using BIGNUM_Ptr = std::unique_ptr<BIGNUM, decltype(&::BN_free)>;
 
 /** @brief Constructor to put object onto bus at a dbus path.
  *  @param[in] bus - Bus to attach to.
@@ -83,5 +95,323 @@ void Manager::delete_()
         certificatePtr.reset(nullptr);
     }
 }
+
+std::string Manager::generateCSR(
+    std::vector<std::string> alternativeNames, std::string challengePassword,
+    std::string city, std::string commonName, std::string contactPerson,
+    std::string country, std::string email, std::string givenName,
+    std::string initials, int64_t keyBitLength, std::string keyCurveId,
+    std::string keyPairAlgorithm, std::vector<std::string> keyUsage,
+    std::string organization, std::string organizationalUnit, std::string state,
+    std::string surname, std::string unstructuredName)
+{
+
+    // set version of x509 req
+    int nVersion = 1;
+    X509_REQ_Ptr x509Req(X509_REQ_new(), ::X509_REQ_free);
+    int ret = X509_REQ_set_version(x509Req.get(), nVersion);
+    if (ret != 1)
+    {
+        log<level::ERR>("Error occured during X509_REQ_set_version call");
+        elog<InternalFailure>();
+    }
+
+    // set subject of x509 req
+    X509_NAME* x509Name = X509_REQ_get_subject_name(x509Req.get());
+
+    for (auto& name : alternativeNames)
+    {
+        ret = X509_NAME_add_entry_by_txt(
+            x509Name, "subjectAltName", MBSTRING_ASC,
+            (const unsigned char*)name.c_str(), -1, -1, 0);
+        if (ret != 1)
+        {
+            log<level::ERR>("Unable to set subjectAltName",
+                            entry("NAME=%s", name.c_str()));
+            elog<InternalFailure>();
+        }
+    }
+    // challengePassword
+    ret = X509_NAME_add_entry_by_txt(
+        x509Name, "challengePassword", MBSTRING_ASC,
+        (const unsigned char*)challengePassword.c_str(), -1, -1, 0);
+    if (ret != 1)
+    {
+        log<level::ERR>(
+            "Unable to set challengePassword",
+            entry("CHALLENGEPASSWORD=%s", challengePassword.c_str()));
+        elog<InternalFailure>();
+    }
+
+    ret = X509_NAME_add_entry_by_txt(x509Name, "L", MBSTRING_ASC,
+                                     (const unsigned char*)city.c_str(), -1, -1,
+                                     0);
+    if (ret != 1)
+    {
+        log<level::ERR>("Unable to set CITY", entry("CITY=%s", city.c_str()));
+        elog<InternalFailure>();
+    }
+    ret = X509_NAME_add_entry_by_txt(x509Name, "CN", MBSTRING_ASC,
+                                     (const unsigned char*)commonName.c_str(),
+                                     -1, -1, 0);
+    if (ret != 1)
+    {
+        log<level::ERR>("Unable to set commonName",
+                        entry("COMMONNAME=%s", commonName.c_str()));
+        elog<InternalFailure>();
+    }
+
+    ret = X509_NAME_add_entry_by_txt(
+        x509Name, "name", MBSTRING_ASC,
+        (const unsigned char*)contactPerson.c_str(), -1, -1, 0);
+    if (ret != 1)
+    {
+        log<level::ERR>("Unable to set contactPerson",
+                        entry("CONTACTPERSON=%s", contactPerson.c_str()));
+        elog<InternalFailure>();
+    }
+    ret = X509_NAME_add_entry_by_txt(x509Name, "C", MBSTRING_ASC,
+                                     (const unsigned char*)country.c_str(), -1,
+                                     -1, 0);
+    if (ret != 1)
+    {
+        log<level::ERR>("Unable to set country",
+                        entry("COUNTRY=%s", country.c_str()));
+        elog<InternalFailure>();
+    }
+
+    ret = X509_NAME_add_entry_by_txt(x509Name, "emailAddress", MBSTRING_ASC,
+                                     (const unsigned char*)email.c_str(), -1,
+                                     -1, 0);
+    if (ret != 1)
+    {
+        log<level::ERR>("Unable to set email",
+                        entry("EMAIL=%s", email.c_str()));
+        elog<InternalFailure>();
+    }
+
+    ret = X509_NAME_add_entry_by_txt(x509Name, "GN", MBSTRING_ASC,
+                                     (const unsigned char*)givenName.c_str(),
+                                     -1, -1, 0);
+    if (ret != 1)
+    {
+        log<level::ERR>("Unable to set givenName",
+                        entry("GIVENNAME=%s", givenName.c_str()));
+        elog<InternalFailure>();
+    }
+    // "initials" **
+    /*ret = X509_NAME_add_entry_by_txt(x509Name, "initials", MBSTRING_ASC,
+                                     (const unsigned char*)initials.c_str(), -1,
+                                     -1, 0);
+    if (ret != 1)
+    {
+        log<level::ERR>("Unable to set initials", entry("INITIALS=%s",
+    initials.c_str())); elog<InternalFailure>();
+    }*/
+
+    // keyPairAlgorithm algorithm
+    ret = X509_NAME_add_entry_by_txt(
+        x509Name, "algorithm", MBSTRING_ASC,
+        (const unsigned char*)keyPairAlgorithm.c_str(), -1, -1, 0);
+    if (ret != 1)
+    {
+        log<level::ERR>("Unable to set keyPairAlgorithm",
+                        entry("KEYPAIRALGORITHM=%s", keyPairAlgorithm.c_str()));
+        elog<InternalFailure>();
+    }
+    // keyUsage
+    for (auto& usage : keyUsage)
+    {
+        ret = X509_NAME_add_entry_by_txt(x509Name, "keyUsage", MBSTRING_ASC,
+                                         (const unsigned char*)usage.c_str(),
+                                         -1, -1, 0);
+        if (ret != 1)
+        {
+            log<level::ERR>("Unable to set keyUsage",
+                            entry("KEYUSAGE=%s", usage.c_str()));
+            elog<InternalFailure>();
+        }
+    }
+    // organizationName
+    ret = X509_NAME_add_entry_by_txt(x509Name, "O", MBSTRING_ASC,
+                                     (const unsigned char*)organization.c_str(),
+                                     -1, -1, 0);
+    if (ret != 1)
+    {
+        log<level::ERR>("Unable to set organization",
+                        entry("ORGANIZATION=%s", organization.c_str()));
+        elog<InternalFailure>();
+    }
+    // organizationalUnitName
+    ret = X509_NAME_add_entry_by_txt(
+        x509Name, "OU", MBSTRING_ASC,
+        (const unsigned char*)organizationalUnit.c_str(), -1, -1, 0);
+    if (ret != 1)
+    {
+        log<level::ERR>(
+            "Unable to set organizationalUnit",
+            entry("ORGANIZATIONUNIT=%s", organizationalUnit.c_str()));
+        elog<InternalFailure>();
+    }
+    ret = X509_NAME_add_entry_by_txt(x509Name, "ST", MBSTRING_ASC,
+                                     (const unsigned char*)state.c_str(), -1,
+                                     -1, 0);
+    if (ret != 1)
+    {
+        log<level::ERR>("Unable to set state",
+                        entry("STATE=%s", state.c_str()));
+        elog<InternalFailure>();
+    }
+    // surname
+    ret = X509_NAME_add_entry_by_txt(x509Name, "SN", MBSTRING_ASC,
+                                     (const unsigned char*)surname.c_str(), -1,
+                                     -1, 0);
+    if (ret != 1)
+    {
+        log<level::ERR>("Unable to set surname",
+                        entry("SURNAME=%s", surname.c_str()));
+        elog<InternalFailure>();
+    }
+    // unstructuredName
+    ret = X509_NAME_add_entry_by_txt(
+        x509Name, "unstructuredName", MBSTRING_ASC,
+        (const unsigned char*)unstructuredName.c_str(), -1, -1, 0);
+    if (ret != 1)
+    {
+        log<level::ERR>("Unable to set unstructuredName",
+                        entry("UNSTRUCTUREDNAME=%s", unstructuredName.c_str()));
+        elog<InternalFailure>();
+    }
+
+    // generate rsa key
+    BIGNUM_Ptr bne(BN_new(), ::BN_free);
+    ret = BN_set_word(bne.get(), RSA_F4);
+    if (ret != 1)
+    {
+        log<level::ERR>("Error occured during BN_set_word call");
+        elog<InternalFailure>();
+    }
+
+    RSA* r = RSA_new();
+    ret = RSA_generate_key_ex(r, keyBitLength, bne.get(), NULL);
+    if (ret != 1)
+    {
+        log<level::ERR>("Error occured during RSA_generate_key_ex call");
+        elog<InternalFailure>();
+    }
+
+    // set public key of x509 req
+    EVP_PKEY_Ptr pKey(EVP_PKEY_new(), ::EVP_PKEY_free);
+    EVP_PKEY_assign_RSA(pKey.get(), r);
+    r = NULL;
+
+    ret = X509_REQ_set_pubkey(x509Req.get(), pKey.get());
+    if (ret != 1)
+    {
+        log<level::ERR>("Error occured while setting private key");
+        elog<InternalFailure>();
+    }
+    // write private key to file
+    std::string privKeyPath = certInstallPath + '/' + PRIV_KEY_FILE_NAME;
+    FILE* fp = fopen(privKeyPath.c_str(), "w");
+    ret = PEM_write_PrivateKey(fp, pKey.get(), NULL, NULL, 0, 0, NULL);
+    if (ret != 1)
+    {
+        log<level::ERR>("Error occured while storing private key to file");
+        elog<InternalFailure>();
+    }
+    if (fp != NULL)
+    {
+        std::fclose(fp);
+    }
+
+    // set sign key of x509 req
+    ret = X509_REQ_sign(x509Req.get(), pKey.get(), EVP_sha256());
+    if (ret <= 0)
+    {
+        log<level::ERR>("Error occured while signing key of x509");
+        elog<InternalFailure>();
+    }
+    std::string csrFilePath = certInstallPath + '/' + CSR_FILE_NAME;
+    ret = saveToFile(csrFilePath, x509Req);
+    if (ret <= 0)
+    {
+        log<level::ERR>("Error occured while saving CSR file");
+        elog<InternalFailure>();
+        return "";
+    }
+    return csrFilePath;
+}
+
+std::string Manager::cSR()
+{
+
+    if (fs::exists(CSR_FILE_NAME))
+    {
+        FILE* fp = std::fopen(CSR_FILE_NAME, "r");
+        X509_REQ_Ptr x509Req(PEM_read_X509_REQ(fp, NULL, NULL, NULL),
+                             ::X509_REQ_free);
+        if (fp != NULL)
+        {
+            std::fclose(fp);
+        }
+
+        BIO_Ptr bio(BIO_new(BIO_s_mem()), ::BIO_free_all);
+        int ret = PEM_write_bio_X509_REQ(bio.get(), x509Req.get());
+        if (ret <= 0)
+        {
+            log<level::ERR>("Error occured while signing key of x509");
+            elog<InternalFailure>();
+        }
+        BUF_MEM* mem = NULL;
+        BIO_get_mem_ptr(bio.get(), &mem);
+        std::string pem(mem->data, mem->length);
+        return pem;
+    }
+
+    return "";
+}
+
+int Manager::saveToFile(const std::string& filePath,
+                        const X509_REQ_Ptr& x509Req)
+{
+    if (fs::exists(filePath))
+    {
+        if (!fs::remove(filePath))
+        {
+            log<level::ERR>("Unable to remove the CSR file",
+                            entry("FILENAME=%s", filePath.c_str()));
+            elog<InternalFailure>();
+            return false;
+        }
+    }
+
+    FILE* fp = NULL;
+    if ((fp = std::fopen(filePath.c_str(), "w")) == NULL)
+    {
+        log<level::ERR>("Error opening the file to write the CSR",
+                        entry("FILENAME=%s", filePath.c_str()));
+        elog<InternalFailure>();
+        return false;
+    }
+
+    /*BIO_Ptr out(BIO_new_file(CSR_FILE, "w"), ::BIO_free_all);
+    rc = PEM_write_bio_X509_REQ(out.get(), x509Req.get());*/
+
+    int rc = PEM_write_X509_REQ(fp, x509Req.get());
+    if (!rc)
+    {
+        log<level::ERR>("Error writing CSR to file",
+                        entry("FILENAME=%s", filePath.c_str()));
+        elog<InternalFailure>();
+        return false;
+    }
+    if (fp != NULL)
+    {
+        fclose(fp);
+    }
+    return true;
+}
+
 } // namespace certs
 } // namespace phosphor
--- certs_manager.hpp
+++ certs_manager.hpp
@@ -1,17 +1,25 @@
 #pragma once
+#include "config.h"
+
 #include "certificate.hpp"
 
+#include <openssl/x509v3.h>
+
+#include <xyz/openbmc_project/Certs/CSR/Create/server.hpp>
+#include <xyz/openbmc_project/Certs/CSR/View/server.hpp>
 #include <xyz/openbmc_project/Certs/Install/server.hpp>
 #include <xyz/openbmc_project/Object/Delete/server.hpp>
-
 namespace phosphor
 {
 namespace certs
 {
 using Create = sdbusplus::xyz::openbmc_project::Certs::server::Install;
 using Delete = sdbusplus::xyz::openbmc_project::Object::server::Delete;
-using Ifaces = sdbusplus::server::object::object<Create, Delete>;
-
+using CSRCreate = sdbusplus::xyz::openbmc_project::Certs::CSR::server::Create;
+using CSRView = sdbusplus::xyz::openbmc_project::Certs::CSR::server::View;
+using Ifaces =
+    sdbusplus::server::object::object<Create, CSRCreate, CSRView, Delete>;
+using X509_REQ_Ptr = std::unique_ptr<X509_REQ, decltype(&::X509_REQ_free)>;
 class Manager : public Ifaces
 {
   public:
@@ -56,7 +64,92 @@ class Manager : public Ifaces
      */
     void delete_() override;
 
+    /** @brief Implementation for GenerateCSR
+     *
+     *  @param[in] alternativeNames - Additional hostnames of the component that
+     *  is being secured.
+     *  @param[in] challengePassword - The challenge password to be applied to
+     *  the certificate for revocation requests.
+     *  @param[in] city - The city or locality of the organization making the
+     *  request. For Example Austin
+     *  @param[in] commonName - The fully qualified domain name of the component
+     *  that is being secured.
+     *  @param[in] contactPerson - The name of the user making the request.
+     *  @param[in] country - The country of the organization making the request.
+     *  @param[in] email - The email address of the contact within the
+     *  organization making the request. Email validtaion does not perform
+     *  authentication to validate the email address. Only check the given email
+     *  format is valid for an email address.
+     *  @param[in] givenName - The given name of the user making the request.
+     *  @param[in] initials - The initials of the user making the request.
+     *  @param[in] keyBitLength - The length of the key in bits, if needed based
+                   on the value of the KeyPairAlgorithm parameter.
+     *  @param[in] keyCurveId - The curve ID to be used with the key, if needed
+     *  based on the value of the KeyPairAlgorithm parameter.
+     *  @param[in] keyPairAlgorithm - The type of key pair for use with signing
+     *  algorithms. Valid built-in algorithm names for private key generation
+     *  are: RSA and EC.
+     *  @param[in] keyUsage - Key usage extensions define the purpose of the
+     *  public key contained in a certificate. Valid Key usage extensions and
+     *  its usage description.
+     *  - ClientAuthentication: The public key is used for TLS WWW client
+     *    authentication.
+     *  - CodeSigning: The public key is used for the signing of executable code
+     *  - CRLSigning: The public key is used for verifying signatures on
+     *    certificate revocation lists (CLRs).
+     *  - DataEncipherment: The public key is used for directly enciphering
+     *    raw user data without the use of an intermediate symmetric cipher.
+     *  - DecipherOnly: The public key could be used for deciphering data while
+     *    performing key agreement.
+     *  - DigitalSignature: The public key is used for verifying digital
+     *    signatures, other than signatures on certificatesand CRLs.
+     *  - EmailProtection: The public key is used for email protection.
+     *  - EncipherOnly: Thepublic key could be used for enciphering data while
+     *    performing key agreement.
+     *  - KeyCertSign: The public key is used for verifying signatures on
+     *    public key certificates.
+     *  - KeyEncipherment: The public key is used for enciphering private or
+     *    secret keys.
+     *  - NonRepudiation: The public key is used to verify digital signatures,
+     *    other than signatures on certificates and CRLs, and used to provide
+     *    a non- repudiation service that protects against the signing entity
+     *    falsely denying some action.
+     *  - OCSPSigning: The public key is used for signing OCSP responses.
+     *  - ServerAuthentication: The public key is used for TLS WWW server
+     *    authentication.
+     *  - Timestamping: The public key is used for binding the hash of an
+     *    object to a time.
+     *  @param[in] organization - The legal name of the organization. This
+     *  should not be abbreviated and should include suffixes such as Inc,Corp,
+     *  or LLC.For example, IBM Corp.
+     *  @param[in] organizationalUnit - The name of the unit or division of the
+     *  organization making the request.
+     *  @param[in] state - The state or province where the organization is
+     *  located. This should not be abbreviated. For example, Texas.
+     *  @param[in] surname - The surname of the user making the request.
+     *  @param[in] unstructuredName - The unstructured name of the subject.
+     *
+     *  @return path[std::string] - The object path of the D-Bus object
+     *  representing CSR string. Note: For new CSR request will update the
+     *  existing CSR in the system.
+     */
+    std::string generateCSR(
+        std::vector<std::string> alternativeNames,
+        std::string challengePassword, std::string city, std::string commonName,
+        std::string contactPerson, std::string country, std::string email,
+        std::string givenName, std::string initials, int64_t keyBitLength,
+        std::string keyCurveId, std::string keyPairAlgorithm,
+        std::vector<std::string> keyUsage, std::string organization,
+        std::string organizationalUnit, std::string state, std::string surname,
+        std::string unstructuredName) override;
+
+    /** @brief Return CSR
+     */
+    std::string cSR() override;
+
   private:
+    int saveToFile(const std::string& filePath, const X509_REQ_Ptr& x509Req);
+
     /** @brief sdbusplus handler */
     sdbusplus::bus::bus& bus;
 
