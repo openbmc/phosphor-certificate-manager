99e43bdb161570830f4cf65dfab89a5ae1d6d5f1
Marri Devender Rao <devenrao@in.ibm.com>
2019-06-25 05:32:56 -0500

    Watch on certificate file added and replaced
    Self signed certificates are created when no certificate
    is present or when uploaded certificate fails the validation.
    
    Certificate manager need to watch on self-signed certificates
    created so that it can create certificate objects or update
    certificate objects.
    
    For CSR certificate private key will not be present catered
    for appending private key from the system.
    
    Tested:
    Test case 1
    1) Ensure no certificate is present
    2) Restart certificate service
    3) Restart bmcweb service
    4) Verify that certificate object is created for the
       self-signed certificate created by bmcweb.
    
    Test case 2
    1) After a certificate is present
    2) modify the bmcweb certificate by replacing it
       with a valid certificate.
    3) verify that certificate manager is notified
    
    Test case 3
    1) Upload CSR based certificate file
    2) Verify that private key is appended to the file
    
    Signed-off-by: Marri Devender Rao <devenrao@in.ibm.com>
    Change-Id: I69c75ccd50cb479a424a481563787a27c96c6fc5

--- certificate.cpp
+++ certificate.cpp
@@ -1,44 +1,21 @@
 #include "certificate.hpp"
 
-#include <openssl/bio.h>
-#include <openssl/crypto.h>
-#include <openssl/err.h>
-#include <openssl/evp.h>
 #include <openssl/pem.h>
 #include <openssl/x509v3.h>
+#include <unistd.h>
 
-#include <fstream>
+#include <filesystem>
 #include <phosphor-logging/elog-errors.hpp>
-#include <xyz/openbmc_project/Certs/error.hpp>
 #include <xyz/openbmc_project/Common/error.hpp>
-
 namespace phosphor
 {
 namespace certs
 {
-// RAII support for openSSL functions.
-using BIO_MEM_Ptr = std::unique_ptr<BIO, decltype(&::BIO_free)>;
-using X509_STORE_CTX_Ptr =
-    std::unique_ptr<X509_STORE_CTX, decltype(&::X509_STORE_CTX_free)>;
-using X509_LOOKUP_Ptr =
-    std::unique_ptr<X509_LOOKUP, decltype(&::X509_LOOKUP_free)>;
-using ASN1_TIME_ptr = std::unique_ptr<ASN1_TIME, decltype(&ASN1_STRING_free)>;
-using EVP_PKEY_Ptr = std::unique_ptr<EVP_PKEY, decltype(&::EVP_PKEY_free)>;
-using BUF_MEM_Ptr = std::unique_ptr<BUF_MEM, decltype(&::BUF_MEM_free)>;
+using namespace phosphor::logging;
+namespace fs = std::filesystem;
+
 using InternalFailure =
     sdbusplus::xyz::openbmc_project::Common::Error::InternalFailure;
-using InvalidCertificate =
-    sdbusplus::xyz::openbmc_project::Certs::Error::InvalidCertificate;
-using Reason = xyz::openbmc_project::Certs::InvalidCertificate::REASON;
-
-// Trust chain related errors.`
-#define TRUST_CHAIN_ERR(errnum)                                                \
-    ((errnum == X509_V_ERR_DEPTH_ZERO_SELF_SIGNED_CERT) ||                     \
-     (errnum == X509_V_ERR_SELF_SIGNED_CERT_IN_CHAIN) ||                       \
-     (errnum == X509_V_ERR_UNABLE_TO_GET_ISSUER_CERT_LOCALLY) ||               \
-     (errnum == X509_V_ERR_CERT_UNTRUSTED) ||                                  \
-     (errnum == X509_V_ERR_UNABLE_TO_VERIFY_LEAF_SIGNATURE))
-
 // Refer to schema 2018.3
 // http://redfish.dmtf.org/schemas/v1/Certificate.json#/definitions/KeyUsage for
 // supported KeyUsage types in redfish
@@ -67,190 +44,56 @@ std::map<uint8_t, std::string> extendedKeyUsageToRfStr = {
     {NID_ad_timeStamping, "Timestamping"},
     {NID_code_sign, "CodeSigning"}};
 
-Certificate::Certificate(sdbusplus::bus::bus& bus, const std::string& objPath,
+Certificate::Certificate(sdbusplus::bus::bus& bus, sdeventplus::Event& event,
+                         const std::string& objPath,
                          const CertificateType& type,
                          const UnitsToRestart& unit,
-                         const CertInstallPath& installPath,
-                         const CertUploadPath& uploadPath,
-                         bool isSkipUnitReload) :
+                         const CertInstallPath& installPath) :
     CertIfaces(bus, objPath.c_str(), true),
-    bus(bus), objectPath(objPath), certType(type), unitToRestart(unit),
-    certInstallPath(installPath)
+    bus(bus), event(event), objectPath(objPath), certType(type),
+    unitToRestart(unit), certInstallPath(installPath), keyHandler(type)
 {
-    auto installHelper = [this](const auto& filePath) {
-        if (!compareKeys(filePath))
-        {
-            elog<InvalidCertificate>(
-                Reason("Private key does not match the Certificate"));
-        };
-    };
-    typeFuncMap[SERVER] = installHelper;
-    typeFuncMap[CLIENT] = installHelper;
-    typeFuncMap[AUTHORITY] = [](auto filePath) {};
-    install(uploadPath, isSkipUnitReload);
-    this->emit_object_added();
-}
+    // Parse the certificate file and populate properties
+    populateProperties();
+    // For existing file add watch on the certificate file
+    modifyWatchPtr = std::make_unique<Watch>(
+        event, certInstallPath, certInstallPath, IN_CLOSE_WRITE, true, [this]() {
+            try
+            {
+                populateProperties();
+            }
+            catch (const InternalFailure& e)
+            {
+                commit<InternalFailure>();
+            }
+        });
 
-Certificate::~Certificate()
-{
-    if (!fs::remove(certInstallPath))
-    {
-        log<level::INFO>("Certificate file not found!",
-                         entry("PATH=%s", certInstallPath.c_str()));
-    }
-    else if (!unitToRestart.empty())
-    {
-        reloadOrReset(unitToRestart);
-    }
+    this->emit_object_added();
 }
 
 void Certificate::replace(const std::string filePath)
 {
-    install(filePath, false);
-}
-
-void Certificate::install(const std::string& filePath, bool isSkipUnitReload)
-{
-    log<level::INFO>("Certificate install ",
-                     entry("FILEPATH=%s", filePath.c_str()));
-    auto errCode = X509_V_OK;
-
-    // Verify the certificate file
-    fs::path file(filePath);
-    if (!fs::exists(file))
+    log<level::ERR>("Certificate replace",
+                    entry("FILE_NAME=%s", filePath.c_str()));
+    if (!fs::exists(filePath))
     {
-        log<level::ERR>("File is Missing", entry("FILE=%s", filePath.c_str()));
+        log<level::ERR>("Input file not existing",
+                        entry("FILE_NAME=%s", filePath.c_str()));
         elog<InternalFailure>();
+        return;
     }
 
+    // validate the certificate
+    keyHandler.verify(filePath);
+
+    // stop watch as user is copying the certificate
+    modifyWatchPtr->stopWatch();
+
+    // Copy the certificate to the installation path
     try
     {
-        if (fs::file_size(filePath) == 0)
-        {
-            // file is empty
-            log<level::ERR>("File is empty",
-                            entry("FILE=%s", filePath.c_str()));
-            elog<InvalidCertificate>(Reason("File is empty"));
-        }
-    }
-    catch (const fs::filesystem_error& e)
-    {
-        // Log Error message
-        log<level::ERR>(e.what(), entry("FILE=%s", filePath.c_str()));
-        elog<InternalFailure>();
-    }
-
-    // Defining store object as RAW to avoid double free.
-    // X509_LOOKUP_free free up store object.
-    // Create an empty X509_STORE structure for certificate validation.
-    auto x509Store = X509_STORE_new();
-    if (!x509Store)
-    {
-        log<level::ERR>("Error occured during X509_STORE_new call");
-        elog<InternalFailure>();
-    }
-
-    OpenSSL_add_all_algorithms();
-
-    // ADD Certificate Lookup method.
-    X509_LOOKUP_Ptr lookup(X509_STORE_add_lookup(x509Store, X509_LOOKUP_file()),
-                           ::X509_LOOKUP_free);
-    if (!lookup)
-    {
-        // Normally lookup cleanup function interanlly does X509Store cleanup
-        // Free up the X509Store.
-        X509_STORE_free(x509Store);
-        log<level::ERR>("Error occured during X509_STORE_add_lookup call");
-        elog<InternalFailure>();
-    }
-    // Load Certificate file.
-    errCode = X509_LOOKUP_load_file(lookup.get(), filePath.c_str(),
-                                    X509_FILETYPE_PEM);
-    if (errCode != 1)
-    {
-        log<level::ERR>("Error occured during X509_LOOKUP_load_file call",
-                        entry("FILE=%s", filePath.c_str()));
-        elog<InvalidCertificate>(Reason("Invalid certificate file format"));
-    }
-
-    // Load Certificate file into the X509 structre.
-    X509_Ptr cert = std::move(loadCert(filePath));
-    X509_STORE_CTX_Ptr storeCtx(X509_STORE_CTX_new(), ::X509_STORE_CTX_free);
-    if (!storeCtx)
-    {
-        log<level::ERR>("Error occured during X509_STORE_CTX_new call",
-                        entry("FILE=%s", filePath.c_str()));
-        elog<InternalFailure>();
-    }
-
-    errCode = X509_STORE_CTX_init(storeCtx.get(), x509Store, cert.get(), NULL);
-    if (errCode != 1)
-    {
-        log<level::ERR>("Error occured during X509_STORE_CTX_init call",
-                        entry("FILE=%s", filePath.c_str()));
-        elog<InternalFailure>();
-    }
-
-    // Set time to current time.
-    auto locTime = time(nullptr);
-
-    X509_STORE_CTX_set_time(storeCtx.get(), X509_V_FLAG_USE_CHECK_TIME,
-                            locTime);
-
-    errCode = X509_verify_cert(storeCtx.get());
-    if (errCode == 1)
-    {
-        errCode = X509_V_OK;
-    }
-    else if (errCode == 0)
-    {
-        errCode = X509_STORE_CTX_get_error(storeCtx.get());
-        log<level::ERR>("Certificate verification failed",
-                        entry("FILE=%s", filePath.c_str()),
-                        entry("ERRCODE=%d", errCode));
-    }
-    else
-    {
-        log<level::ERR>("Error occured during X509_verify_cert call",
-                        entry("FILE=%s", filePath.c_str()));
-        elog<InternalFailure>();
-    }
-
-    // Allow certificate upload, for "certificate is not yet valid" and
-    // trust chain related errors.
-    if (!((errCode == X509_V_OK) ||
-          (errCode == X509_V_ERR_CERT_NOT_YET_VALID) ||
-          TRUST_CHAIN_ERR(errCode)))
-    {
-        if (errCode == X509_V_ERR_CERT_HAS_EXPIRED)
-        {
-            elog<InvalidCertificate>(Reason("Expired Certificate"));
-        }
-        // Loging general error here.
-        elog<InvalidCertificate>(Reason("Certificate validation failed"));
-    }
-
-    // Invoke type specific compare keys function.
-    auto iter = typeFuncMap.find(certType);
-    if (iter == typeFuncMap.end())
-    {
-        log<level::ERR>("Unsupported Type", entry("TYPE=%s", certType.c_str()));
-        elog<InternalFailure>();
-    }
-    iter->second(filePath);
-
-    // Copy thecertificate to the installation path
-    auto path = fs::path(certInstallPath).parent_path();
-    try
-    {
-        fs::create_directories(path);
-        // During bootup will be parsing existing file so no need to
-        // copy it.
-        if (filePath != certInstallPath)
-        {
-            fs::copy_file(filePath, certInstallPath,
-                          fs::copy_options::overwrite_existing);
-        }
+        fs::copy_file(filePath, certInstallPath,
+                      fs::copy_options::overwrite_existing);
     }
     catch (fs::filesystem_error& e)
     {
@@ -260,22 +103,23 @@ void Certificate::install(const std::string& filePath, bool isSkipUnitReload)
         elog<InternalFailure>();
     }
 
-    if (!isSkipUnitReload)
-    {
-        // restart the units
-        if (!unitToRestart.empty())
-        {
-            reloadOrReset(unitToRestart);
-        }
-    }
-
     // Parse the certificate file and populate properties
     populateProperties();
+
+    // as certificate is replaced notify consumers to reload SSL context
+    reloadOrReset();
+
+    // start watch again to check for files being over-written by other apps
+    modifyWatchPtr->startWatch();
 }
 
 void Certificate::populateProperties()
 {
-    X509_Ptr cert = std::move(loadCert(certInstallPath));
+    using BIO_MEM_Ptr = std::unique_ptr<BIO, decltype(&::BIO_free)>;
+    using BUF_MEM_Ptr = std::unique_ptr<BUF_MEM, decltype(&::BUF_MEM_free)>;
+    using ASN1_TIME_ptr =
+        std::unique_ptr<ASN1_TIME, decltype(&ASN1_STRING_free)>;
+    X509_Ptr cert = std::move(keyHandler.loadCert(certInstallPath));
     // Update properties if no error thrown
     BIO_MEM_Ptr certBio(BIO_new(BIO_s_mem()), BIO_free);
     PEM_write_bio_X509(certBio.get(), cert.get());
@@ -352,120 +196,28 @@ void Certificate::populateProperties()
     CertificateIface::validNotBefore((days * dayToSeconds) + secs);
 }
 
-X509_Ptr Certificate::loadCert(const std::string& filePath)
+void Certificate::reloadOrReset()
 {
-    log<level::INFO>("Certificate loadCert",
-                     entry("FILEPATH=%s", filePath.c_str()));
-    // Read Certificate file
-    X509_Ptr cert(X509_new(), ::X509_free);
-    if (!cert)
+    if (!unitToRestart.empty())
     {
-        log<level::ERR>("Error occured during X509_new call",
-                        entry("FILE=%s", filePath.c_str()),
-                        entry("ERRCODE=%lu", ERR_get_error()));
-        elog<InternalFailure>();
-    }
-
-    BIO_MEM_Ptr bioCert(BIO_new_file(filePath.c_str(), "rb"), ::BIO_free);
-    if (!bioCert)
-    {
-        log<level::ERR>("Error occured during BIO_new_file call",
-                        entry("FILE=%s", filePath.c_str()));
-        elog<InternalFailure>();
-    }
-
-    X509* x509 = cert.get();
-    if (!PEM_read_bio_X509(bioCert.get(), &x509, nullptr, nullptr))
-    {
-        log<level::ERR>("Error occured during PEM_read_bio_X509 call",
-                        entry("FILE=%s", filePath.c_str()));
-        elog<InternalFailure>();
-    }
-    return cert;
-}
-bool Certificate::compareKeys(const std::string& filePath)
-{
-    log<level::INFO>("Certificate compareKeys",
-                     entry("FILEPATH=%s", filePath.c_str()));
-    X509_Ptr cert(X509_new(), ::X509_free);
-    if (!cert)
-    {
-        log<level::ERR>("Error occured during X509_new call",
-                        entry("FILE=%s", filePath.c_str()),
-                        entry("ERRCODE=%lu", ERR_get_error()));
-        elog<InternalFailure>();
-    }
-
-    BIO_MEM_Ptr bioCert(BIO_new_file(filePath.c_str(), "rb"), ::BIO_free);
-    if (!bioCert)
-    {
-        log<level::ERR>("Error occured during BIO_new_file call",
-                        entry("FILE=%s", filePath.c_str()));
-        elog<InternalFailure>();
-    }
-
-    X509* x509 = cert.get();
-    PEM_read_bio_X509(bioCert.get(), &x509, nullptr, nullptr);
-
-    EVP_PKEY_Ptr pubKey(X509_get_pubkey(cert.get()), ::EVP_PKEY_free);
-    if (!pubKey)
-    {
-        log<level::ERR>("Error occurred during X509_get_pubkey",
-                        entry("FILE=%s", filePath.c_str()),
-                        entry("ERRCODE=%lu", ERR_get_error()));
-        elog<InvalidCertificate>(Reason("Failed to get public key info"));
-    }
-
-    BIO_MEM_Ptr keyBio(BIO_new(BIO_s_file()), ::BIO_free);
-    if (!keyBio)
-    {
-        log<level::ERR>("Error occured during BIO_s_file call",
-                        entry("FILE=%s", filePath.c_str()));
-        elog<InternalFailure>();
-    }
-    BIO_read_filename(keyBio.get(), filePath.c_str());
-
-    EVP_PKEY_Ptr priKey(
-        PEM_read_bio_PrivateKey(keyBio.get(), nullptr, nullptr, nullptr),
-        ::EVP_PKEY_free);
-    if (!priKey)
-    {
-        log<level::ERR>("Error occurred during PEM_read_bio_PrivateKey",
-                        entry("FILE=%s", filePath.c_str()),
-                        entry("ERRCODE=%lu", ERR_get_error()));
-        elog<InvalidCertificate>(Reason("Failed to get private key info"));
-    }
-
-    int32_t rc = EVP_PKEY_cmp(priKey.get(), pubKey.get());
-    if (rc != 1)
-    {
-        log<level::ERR>("Private key is not matching with Certificate",
-                        entry("FILE=%s", filePath.c_str()),
-                        entry("ERRCODE=%d", rc));
-        return false;
-    }
-    return true;
-}
-
-void Certificate::reloadOrReset(const UnitsToRestart& unit)
-{
-    constexpr auto SYSTEMD_SERVICE = "org.freedesktop.systemd1";
-    constexpr auto SYSTEMD_OBJ_PATH = "/org/freedesktop/systemd1";
-    constexpr auto SYSTEMD_INTERFACE = "org.freedesktop.systemd1.Manager";
-    try
-    {
-        auto method =
-            bus.new_method_call(SYSTEMD_SERVICE, SYSTEMD_OBJ_PATH,
-                                SYSTEMD_INTERFACE, "ReloadOrRestartUnit");
-        method.append(unit, "replace");
-        bus.call_noreply(method);
-    }
-    catch (const sdbusplus::exception::SdBusError& e)
-    {
-        log<level::ERR>("Failed to reload or restart service",
-                        entry("ERR=%s", e.what()),
-                        entry("UNIT=%s", unit.c_str()));
-        elog<InternalFailure>();
+        constexpr auto SYSTEMD_SERVICE = "org.freedesktop.systemd1";
+        constexpr auto SYSTEMD_OBJ_PATH = "/org/freedesktop/systemd1";
+        constexpr auto SYSTEMD_INTERFACE = "org.freedesktop.systemd1.Manager";
+        try
+        {
+            auto method =
+                bus.new_method_call(SYSTEMD_SERVICE, SYSTEMD_OBJ_PATH,
+                                    SYSTEMD_INTERFACE, "ReloadOrRestartUnit");
+            method.append(unitToRestart, "replace");
+            bus.call_noreply(method);
+        }
+        catch (const sdbusplus::exception::SdBusError& e)
+        {
+            log<level::ERR>("Failed to reload or restart service",
+                            entry("ERR=%s", e.what()),
+                            entry("UNIT=%s", unitToRestart.c_str()));
+            elog<InternalFailure>();
+        }
     }
 }
 } // namespace certs
---
+++ watch.cpp
@@ -0,0 +1,111 @@
+#include "watch.hpp"
+
+#include <sys/inotify.h>
+#include <unistd.h>
+
+#include <cstring>
+#include <filesystem>
+#include <phosphor-logging/elog-errors.hpp>
+#include <phosphor-logging/elog.hpp>
+#include <xyz/openbmc_project/Common/error.hpp>
+
+namespace phosphor
+{
+namespace certs
+{
+using namespace phosphor::logging;
+namespace fs = std::filesystem;
+using InternalFailure =
+    sdbusplus::xyz::openbmc_project::Common::Error::InternalFailure;
+
+Watch::Watch(sdeventplus::Event& event, const std::string& watch,
+             const std::string& certFile, uint32_t mask, bool recursive,
+             Callback cb) :
+    event(event),
+    watch(watch), certFile(certFile), mask(mask), recursive(recursive),
+    callback(cb)
+{
+    log<level::INFO>("Adding watch ", entry("WATCH=%s", watch.c_str()),
+                     entry("CERT_FIE=%s", certFile.c_str()));
+    startWatch();
+
+    timerPtr = std::make_unique<Timer>(event, [this](Timer&) {
+            this->readCertificate();
+        });
+}
+
+Watch::~Watch()
+{
+    stopWatch();
+}
+
+void Watch::readCertificate()
+{
+    std::error_code ec; //use non throwing file_size method
+    if (fs::exists(certFile) &&  fs::file_size(certFile, ec) > 0)
+    {
+        log<level::INFO>("invoking callback method for certiifate watch");
+        callback();
+    }
+    else
+    {
+        log<level::ERR>("Watch file does not exist",
+                        entry("FILE_NAME=%s", certFile.c_str()));
+    }
+    if (recursive)
+    {
+        // Restart the watch
+        log<level::INFO>("Restart recursive watch");
+        startWatch();
+    }
+}
+
+void Watch::startWatch()
+{
+    fd = inotify_init1(IN_NONBLOCK);
+    if (-1 == fd)
+    {
+        log<level::ERR>("inotify_init1 failed,",
+                        entry("ERR=%s", std::strerror(errno)));
+        elog<InternalFailure>();
+    }
+    wd = inotify_add_watch(fd, watch.c_str(), mask);
+    if (-1 == wd)
+    {
+        close(fd);
+        log<level::ERR>("inotify_add_watch failed,",
+                        entry("ERR=%s", std::strerror(errno)),
+                        entry("WATCH=%s", watch.c_str()));
+        elog<InternalFailure>();
+    }
+
+    ioPtr = std::make_unique<sdeventplus::source::IO>(
+        event, fd, EPOLLIN,
+        [this](sdeventplus::source::IO&, int fd, uint32_t revents) {
+            //stop the watch so no further notifications are received
+            stopWatch();
+
+            //after notification it takes a while for the file to have data
+            std::chrono::seconds delay(3);
+            timerPtr->restartOnce(delay);
+        });
+}
+
+void Watch::stopWatch()
+{
+    if (-1 != fd)
+    {
+        if (-1 != wd)
+        {
+            inotify_rm_watch(fd, wd);
+        }
+        close(fd);
+    }
+    if (ioPtr)
+    {
+        ioPtr.reset();
+    }
+}
+
+} // namespace certs
+} // namespace phosphor
