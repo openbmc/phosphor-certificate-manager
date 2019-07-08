bed59f424aa2378c54e47dc1c02cb7c2800eb35a
Kowalski, Kamil <kamil.kowalski@intel.com>
2019-07-08 17:09:39 +0200

    This request is a proposition of extending current mode=authority with support for multiple certificates instead of single one.
    This review addresses also this issue:
       https://github.com/openbmc/phosphor-certificate-manager/issues/3
    but with a restriction to mode=authority. Other modes sitll operates
    on a single certification file.
    
    New mode requires that user provides directory path instead of certificate path
    as --path argument if using --type=storage.
    
    Tested:
    - Manually tested Install, Remove and Replace paths for existing modes
    to confirm no change of behavior occures (authority, client, server)
    - Manually tested Install, Remove and Replace paths for storage mode
    to confirm that it behaves as expected i.e. filename is changed on certificate
    replacement that mirrors change in certificate hash
    - Confirmed no regression in unit tests
    
    Change-Id: Icd33723c1fc2580679aaaf54b3e99dfb09342402
    Signed-off-by: Kowalski, Kamil <kamil.kowalski@intel.com>
    Signed-off-by: Zbigniew Kurzynski <zbigniew.kurzynski@intel.com>

--- certs_manager.hpp
+++ certs_manager.hpp
@@ -151,11 +151,11 @@ class Manager : public Ifaces
         std::string organizationalUnit, std::string state, std::string surname,
         std::string unstructuredName) override;
 
-    /** @brief Get reference to certificate
+    /** @brief Get reference to certificates' collection
      *
-     *  @return Reference to certificate
+     *  @return Reference to certificates' collection
      */
-    CertificatePtr& getCertificate();
+    std::vector<std::unique_ptr<Certificate>>& getCertificates();
 
   private:
     void generateCSRHelper(std::vector<std::string> alternativeNames,
@@ -213,7 +213,7 @@ class Manager : public Ifaces
     /** @brief Load certifiate
      *  Load certificate and create certificate object
      */
-    void createCertificate();
+    void createCertificates();
 
     /** @brief Create RSA private key file
      *  Create RSA private key file by generating rsa key if not created
@@ -245,8 +245,8 @@ class Manager : public Ifaces
     /** @brief Certificate file installation path **/
     CertInstallPath certInstallPath;
 
-    /** @brief pointer to certificate */
-    CertificatePtr certificatePtr = nullptr;
+    /** @brief Collection of pointers to certificate */
+    std::vector<std::unique_ptr<Certificate>> installedCerts;
 
     /** @brief pointer to CSR */
     std::unique_ptr<CSR> csrPtr = nullptr;
@@ -259,6 +259,9 @@ class Manager : public Ifaces
 
     /** @brif Parent path i.e certificate directory path */
     fs::path certParentInstallPath;
+
+    /** @brief Certificate ID pool */
+    uint64_t certIdCounter;
 };
 } // namespace certs
 } // namespace phosphor
