8ec94ef2dbcaa304ecaa3c47f54f76178df451c2
Ravi Teja <raviteja28031990@gmail.com>
2021-04-20 11:52:42 -0500

    CA Certs: Modify object entry path to fix object sub-tree hierarchy
    Currently there is no interface added signal generated as object
    path is not following hierarchy.
    
    to fix this behaviour, this commit modifies object entry path
    to /xyz/openbmc_project/certs/ca/entry/<id>
    
    InterfacesAdded signal:
    signal time=1619111735.917791 sender=:1.660 -> destination=(null destination)
     serial=68 path=/xyz/openbmc_project/certs/ca; interface=org.freedesktop.DBus.ObjectManager;
      member=InterfacesAdded
       object path "/xyz/openbmc_project/certs/ca/entry/6"
    
    Tested by:
    D-bus monitor while Creating dbus objects
    
    Signed-off-by: Ravi Teja <raviteja28031990@gmail.com>
    Change-Id: I3ce8aa2b2ec5da321557ea40f484ea2947d57442

--- bmc-vmi-ca/mainapp.cpp
+++ bmc-vmi-ca/mainapp.cpp
@@ -8,10 +8,9 @@
 int main(int argc, char** argv)
 {
     auto bus = sdbusplus::bus::new_default();
-    std::string objPath = "/xyz/openbmc_project/certs/ca";
-
+    static constexpr auto objPath = "/xyz/openbmc_project/certs/ca";
     // Add sdbusplus ObjectManager
-    sdbusplus::server::manager::manager objManager(bus, objPath.c_str());
+    sdbusplus::server::manager::manager objManager(bus, objPath);
 
     // Get default event loop
     auto event = sdeventplus::Event::get_default();
@@ -19,7 +18,7 @@ int main(int argc, char** argv)
     // Attach the bus to sd_event to service user requests
     bus.attach_event(event.get(), SD_EVENT_PRIORITY_NORMAL);
 
-    ca::cert::CACertMgr manager(bus, event, objPath.c_str());
+    ca::cert::CACertMgr manager(bus, event, objPath);
 
     std::string busName = "xyz.openbmc_project.Certs.ca.authority.Manager";
     bus.request_name(busName.c_str());
