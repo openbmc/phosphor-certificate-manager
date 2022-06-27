6204e995d825b652e8f461b6cfab67a6426216f9
Lei YU <yulei.sh@bytedance.com>
2022-06-27 14:42:47 +0800

    Allow for expired certificate
    The code throws for an expired certificate, which results in the below
    behavior:
    
    1. If BMC starts when the time is invalid (e.g. the date is in 1970),
    bmcweb will create a default certificate with hostname `testhost`;
    
    2. In later reboots when BMC get a valid time, the bmcweb loads the
    certificate as before. But phosphor-certificate-manager will throw on
    this certificate. Then there is no DBus object created for this
    certificate (`/xyz/openbmc_project/certs/server/https/1`)
    
    3. Due to the missing DBus object:
     * We will not be able to replace the certificate, e.g. by below
     Redfish URI:
    ```
     /redfish/v1/CertificateService/Actions/CertificateService.ReplaceCertificate
    ```
     * When the BMC gets the hostname, bmcweb will generate a new
     self-signed certificate with the hostname and replace it, the
     replacement fails as well.
    
    This commit adds a config option that allows the expired certificate to
    be created on DBus and fixes the above issues and it is enabled by
    default.
    
    Signed-off-by: Lei YU <yulei.sh@bytedance.com>
    Change-Id: Ib02bd686c9bfeb6401b269af20856824647f54c5

--- x509_utils.cpp
+++ x509_utils.cpp
@@ -184,15 +184,24 @@ void validateCertificateAgainstStore(X509_STORE& x509Store, X509& cert)
 
     // Allow certificate upload, for "certificate is not yet valid" and
     // trust chain related errors.
-    if (!((errCode == X509_V_OK) ||
-          (errCode == X509_V_ERR_CERT_NOT_YET_VALID) ||
-          isTrustChainError(errCode)))
+    bool isOK = (errCode == X509_V_OK) ||
+                (errCode == X509_V_ERR_CERT_NOT_YET_VALID) ||
+                isTrustChainError(errCode);
+
+#ifdef ALLOW_EXPIRED
+    // If ALLOW_EXPIRED is defined, allow expired certificate so that it could
+    // be replaced
+    isOK |= (errCode == X509_V_ERR_CERT_HAS_EXPIRED);
+#endif
+    if (!isOK)
     {
+#ifndef ALLOW_EXPIRED
         if (errCode == X509_V_ERR_CERT_HAS_EXPIRED)
         {
             log<level::ERR>("Expired certificate ");
             elog<InvalidCertificate>(Reason("Expired Certificate"));
         }
+#endif
         // Loging general error here.
         log<level::ERR>(
             "Certificate validation failed", entry("ERRCODE=%d", errCode),
