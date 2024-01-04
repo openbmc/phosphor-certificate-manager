25b6f7f090299ecc2e8c24eb13a43ca963e07950
Ravi Teja <raviteja28031990@gmail.com>
2024-01-04 10:08:02 -0600

    Add subjectAltName extensions support
    This commit adds subjectAltName extensions to CSR and avoid populating
    subjectAltName under subject section.
    
    Tested by:
    Generate CSR with given subject alternative names
    noticed below added extension into CSR string
    "Requested Extensions:
    X509v3 Subject Alternative Name:
    DNS:cacrypt-cert-mgt DNS:carcrypt-cert-mgt.in.com"
    
    Change-Id: Ib6ce54f9e5bcc0d43ec4274067c7a8643753e367
    Signed-off-by: Ravi Teja <raviteja28031990@gmail.com>

--- certs_manager.cpp
+++ certs_manager.cpp
@@ -14,6 +14,7 @@
 #include <openssl/opensslv.h>
 #include <openssl/pem.h>
 #include <openssl/rsa.h>
+#include <openssl/x509v3.h>
 #include <unistd.h>
 
 #include <phosphor-logging/elog-errors.hpp>
@@ -547,7 +548,7 @@ void Manager::generateCSRHelper(
     int ret = 0;
 
     // set version of x509 req
-    int nVersion = 1;
+    int nVersion = 3;
     X509ReqPtr x509Req(X509_REQ_new(), ::X509_REQ_free);
     ret = X509_REQ_set_version(x509Req.get(), nVersion);
     if (ret == 0)
@@ -560,13 +561,6 @@ void Manager::generateCSRHelper(
     // set subject of x509 req
     X509_NAME* x509Name = X509_REQ_get_subject_name(x509Req.get());
 
-    if (!alternativeNames.empty())
-    {
-        for (auto& name : alternativeNames)
-        {
-            addEntry(x509Name, "subjectAltName", name);
-        }
-    }
     addEntry(x509Name, "challengePassword", challengePassword);
     addEntry(x509Name, "L", city);
     addEntry(x509Name, "CN", commonName);
@@ -613,6 +607,35 @@ void Manager::generateCSRHelper(
         elog<InvalidArgument>(
             Argument::ARGUMENT_NAME("KEYPAIRALGORITHM"),
             Argument::ARGUMENT_VALUE(keyPairAlgorithm.c_str()));
+    }
+
+    // set subjectAltName extension
+    if (!alternativeNames.empty())
+    {
+        STACK_OF(X509_EXTENSION)* extlist = sk_X509_EXTENSION_new_null();
+
+        std::string altNameStr{};
+        for (auto& name : alternativeNames)
+        {
+            altNameStr += std::format("DNS:{} ", name.data());
+        }
+
+        X509_EXTENSION* ext = X509V3_EXT_conf_nid(NULL, NULL,
+                                  NID_subject_alt_name,
+                                  altNameStr.data());
+        if (ext == nullptr)
+        {
+            lg2::error("Error creating subjectAltName extension");
+            elog<InternalFailure>();
+        }
+
+        sk_X509_EXTENSION_push(extlist, ext);
+        if (!X509_REQ_add_extensions(x509Req.get(), extlist))
+        {
+            lg2::error("Error adding subjectAltName extension to the request");
+            elog<InternalFailure>();
+        }
+        sk_X509_EXTENSION_pop_free(extlist, X509_EXTENSION_free);
     }
 
     ret = X509_REQ_set_pubkey(x509Req.get(), pKey.get());
