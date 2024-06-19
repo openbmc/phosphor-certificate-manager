820b76a62bd5fe72f13586dfad25e6357347a864
Ravi Teja <raviteja28031990@gmail.com>
2024-06-19 04:37:31 -0500

    Fix CI failure
    Change-Id: I1bf26fb2fafbf68bcb128eb77083a99867dd21c8
    Signed-off-by: Ravi Teja <raviteja28031990@gmail.com>

--- mainapp.cpp
+++ mainapp.cpp
@@ -47,7 +47,7 @@ int main(int argc, char** argv)
     {
         std::exit(EXIT_FAILURE);
     }
-
+    
     auto bus = sdbusplus::bus::new_default();
     auto objPath = std::string(objectNamePrefix) + '/' + arguments.typeStr +
                    '/' + arguments.endpoint;
