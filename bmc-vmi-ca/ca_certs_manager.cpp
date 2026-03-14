10d135e743d94fc1d3a83930280f2bf89cd2d10f
Ravi Teja <raviteja28031990@gmail.com>
2026-03-14 03:13:41 -0500

    VMI Cert: Fix SignCSR error handling
    This commit fixes SignCSR error handling
    
    Tested by:
    verified SignCSR with empty string and valid CSR
    
    Change-Id: Ibd2d2e80bfed52292db38c4772af0545f38c03e6
    Signed-off-by: Ravi Teja <raviteja28031990@gmail.com>

--- bmc-vmi-ca/ca_certs_manager.cpp
+++ bmc-vmi-ca/ca_certs_manager.cpp
@@ -26,13 +26,28 @@ sdbusplus::message::object_path CACertMgr::signCSR(std::string csr)
     std::string objPath;
     try
     {
+        if (csr.empty())
+        {
+            lg2::error("CSR string is empty");
+            elog<InvalidArgument>(Argument::ARGUMENT_NAME("CSR"),
+                                  Argument::ARGUMENT_VALUE("Empty CSR"));
+        }
         if (csr.size() > maxCertSize)
         {
-            lg2::error("Invalid CSR size");
+            lg2::error("Invalid CSR size: {SIZE}", "SIZE", csr.size());
             elog<InvalidArgument>(Argument::ARGUMENT_NAME("CSR"),
                                   Argument::ARGUMENT_VALUE(csr.c_str()));
         }
+
         auto id = lastEntryId + 1;
+        if (id == 0)
+        {
+            lg2::error("Entry ID overflow - maximum entries reached");
+            elog<InvalidArgument>(
+                Argument::ARGUMENT_NAME("EntryID"),
+                Argument::ARGUMENT_VALUE("ID overflow"));
+        }
+
         objPath = fs::path(objectNamePrefix) / "ca" / "entry" /
                   std::to_string(id);
         std::string cert;
@@ -41,11 +56,11 @@ sdbusplus::message::object_path CACertMgr::signCSR(std::string csr)
         // the certificate string would be updated with actual certificate.
         entries.insert(std::make_pair(
             id, std::make_unique<Entry>(bus, objPath, id, csr, cert, *this)));
-        lastEntryId++;
+        lastEntryId = id;
     }
     catch (const std::invalid_argument& e)
     {
-        lg2::error(e.what());
+        lg2::error("Invalid argument: {ERR}", "ERR", e.what());
         elog<InvalidArgument>(Argument::ARGUMENT_NAME("csr"),
                               Argument::ARGUMENT_VALUE(csr.c_str()));
     }
