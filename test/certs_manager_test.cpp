fc0db7463ddae4575e86471ef90d7633cfae3c83
Chandramohan Harkude <chandramohan.harkude@gmail.com>
2026-06-28 16:12:09 +0530

    Use QuotaExceeded error for CA certificate limit
    Truststore over-limit CA upload was throwing elog<NotAllowed> which
    maps to a generic HTTP 500 "operation not allowed" response instead of
    a distinct limit error. Replace elog<NotAllowed> with the generic
    elog<QuotaExceeded> in both install() and installAll() paths, and add
    lg2::error logging with installed count and configured max before each
    exception to aid diagnostics.
    
    Depends on:
    https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/+/91794
    
    Change-Id: I3e17d8140a7a8e95ba871fc2d11eb5a3df3d50dc
    Signed-off-by: Chandramohan Harkude <chandramohan.harkude@gmail.com>

--- test/certs_manager_test.cpp
+++ test/certs_manager_test.cpp
@@ -555,8 +555,8 @@ TEST_F(TestCertificates, InvokeInstallAuthCertLimit)
     // Prepare new certificatate
     createNewCertificate(true);
 
-    using NotAllowed =
-        sdbusplus::xyz::openbmc_project::Common::Error::NotAllowed;
+    using QuotaExceeded =
+        sdbusplus::xyz::openbmc_project::Common::Error::QuotaExceeded;
     EXPECT_THROW(
         {
             try
@@ -564,12 +564,12 @@ TEST_F(TestCertificates, InvokeInstallAuthCertLimit)
                 // Try to install one more certificate
                 mainApp.install(certificateFile);
             }
-            catch (const NotAllowed& e)
+            catch (const QuotaExceeded& e)
             {
                 throw;
             }
         },
-        NotAllowed);
+        QuotaExceeded);
 
     // Check that the original certificate has been not removed
     EXPECT_FALSE(fs::is_empty(verifyDir));
@@ -1777,7 +1777,7 @@ TEST_F(AuthoritiesListTest, TooManyRootCertificates)
                           authoritiesListFolder);
     createAuthoritiesList(maxNumAuthorityCertificates + 1);
     EXPECT_THROW(manager.installAll(sourceAuthoritiesListFile),
-                 sdbusplus::xyz::openbmc_project::Common::Error::NotAllowed);
+                 sdbusplus::xyz::openbmc_project::Common::Error::QuotaExceeded);
 }
 
 TEST_F(AuthoritiesListTest, CertInWrongFormat)
