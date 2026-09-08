7382f1ca10b8e724d71feba9790936e5037a1f5a
Tan Siewert <tan.siewert@9elements.com>
2026-09-09 01:09:52 +0200

    certificate: add OpenSSL 4.0 support
    With OpenSSL 4.0, ASN1_STRING and several other types are now opaque [0]
    and cannot be reached like before, causing the build to fail.
    
    Given that Yocto has now migrated to OpenSSL 4.0, the code must be
    compatible with both 3.x and 4.0.
    
    Modify the accesses for ASN1_STRING and X509_NAME to use the appropriate
    functions and construct the type with all options directly, instead of
    assigning them afterwards.
    
    BIO_read_filename's return value must be checked as it is an expression
    now in OpenSSL 4.0.
    
    Tested: Code compiles both with both OpenSSL 3.6.3, as well as 4.0.2,
            and unit tests pass.
    
    References:
    
    [0] https://github.com/openssl/openssl/blob/5ce57ab932b70e030249190a4b5a02232c080eea/CHANGES.md?plain=1#L1523
    
    Change-Id: I197c24d40e4141c726d650b17489cb2fd343344a
    Signed-off-by: Tan Siewert <tan.siewert@9elements.com>

--- certificate.cpp
+++ certificate.cpp
@@ -552,13 +552,13 @@ void Certificate::populateProperties(X509& cert)
 
     BIOMemPtr subBio(BIO_new(BIO_s_mem()), BIO_free);
     // This pointer cannot be freed independently.
-    X509_NAME* sub = X509_get_subject_name(&cert);
+    const X509_NAME* sub = X509_get_subject_name(&cert);
     X509_NAME_print_ex(subBio.get(), sub, 0, XN_FLAG_SEP_COMMA_PLUS);
     subject(readMemoryBio(*subBio));
 
     BIOMemPtr issuerBio(BIO_new(BIO_s_mem()), BIO_free);
     // This pointer cannot be freed independently.
-    X509_NAME* issuerName = X509_get_issuer_name(&cert);
+    const X509_NAME* issuerName = X509_get_issuer_name(&cert);
     X509_NAME_print_ex(issuerBio.get(), issuerName, 0, XN_FLAG_SEP_COMMA_PLUS);
     issuer(readMemoryBio(*issuerBio));
 
@@ -571,11 +571,12 @@ void Certificate::populateProperties(X509& cert)
                            ASN1_BIT_STRING_free);
     if (usage != nullptr)
     {
-        for (auto i = 0; i < usage->length; ++i)
+        const unsigned char* usageData = ASN1_STRING_get0_data(usage.get());
+        for (int i = 0; i < ASN1_STRING_length(usage.get()); ++i)
         {
             for (auto& x : keyUsageToRfStr)
             {
-                if (x.first & usage->data[i])
+                if (x.first & usageData[i])
                 {
                     keyUsageList.push_back(x.second);
                     break;
@@ -607,11 +608,11 @@ void Certificate::populateProperties(X509& cert)
     ASN1_TIME_set_string(epoch.get(), "19700101000000Z");
 
     constexpr uint64_t dayToSeconds = 86400; // 24 * 60 * 60
-    ASN1_TIME* notAfter = X509_get_notAfter(&cert);
+    const ASN1_TIME* notAfter = X509_get0_notAfter(&cert);
     ASN1_TIME_diff(&days, &secs, epoch.get(), notAfter);
     validNotAfter((days * dayToSeconds) + secs);
 
-    ASN1_TIME* notBefore = X509_get_notBefore(&cert);
+    const ASN1_TIME* notBefore = X509_get0_notBefore(&cert);
     ASN1_TIME_diff(&days, &secs, epoch.get(), notBefore);
     validNotBefore((days * dayToSeconds) + secs);
 }
@@ -625,7 +626,12 @@ void Certificate::checkAndAppendPrivateKey(const std::string& filePath)
                    filePath);
         elog<InternalFailure>();
     }
-    BIO_read_filename(keyBio.get(), filePath.c_str());
+    if (BIO_read_filename(keyBio.get(), filePath.c_str()) <= 0)
+    {
+        lg2::error("Error occurred during BIO_read_filename call, FILE:{FILE}",
+                   "FILE", filePath);
+        elog<InternalFailure>();
+    }
 
     EVPPkeyPtr priKey(
         PEM_read_bio_PrivateKey(keyBio.get(), nullptr, nullptr, nullptr),
@@ -712,7 +718,12 @@ bool Certificate::compareKeys(const std::string& filePath)
                    filePath);
         elog<InternalFailure>();
     }
-    BIO_read_filename(keyBio.get(), filePath.c_str());
+    if (BIO_read_filename(keyBio.get(), filePath.c_str()) <= 0)
+    {
+        lg2::error("Error occurred during BIO_read_filename call, FILE:{FILE}",
+                   "FILE", filePath);
+        elog<InternalFailure>();
+    }
 
     EVPPkeyPtr priKey(
         PEM_read_bio_PrivateKey(keyBio.get(), nullptr, nullptr, nullptr),
